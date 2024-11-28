b6164ae5d346bfa91831da1415afcbff3359b061
idoymuhammad <ilhamlenkara@gmail.com>
2024-11-28 13:14:38 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,13 @@
+<?php
+function perkenalan (): void{
+    echo "Hallo nama saya : Muhammad ilham";
+}
+
+function perkalian($angka1,$angka2): int{
+    return $angka1 = $angka2;
+}
+
+perkenalan();
+echo "<br>";
+echo "Umur Saya :";
+echo perkalian(angka1: 9,angka2: 3);
